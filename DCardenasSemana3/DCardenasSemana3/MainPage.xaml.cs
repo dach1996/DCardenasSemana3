cbb0e0d7c075f523fe834c43205d9b51849d59c6
dach1996 <dach19962012@gmail.com>
2020-10-23 21:13:25 -0500

    Agregar archivos de proyecto.

---
+++ DCardenasSemana3/DCardenasSemana3/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DCardenasSemana3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrirVentanaDos_Clicked(object sender, EventArgs e)
+        {
+            String userValidation = "dach";
+            String passValidation = "1996";
+           
+            await Navigation.PushAsync(new Ventana2(txtUser.Text,textPass.Text ));
+        }
+        
+    }
+}
---
+++ DCardenasSemana3/DCardenasSemana3/Ventana2.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DCardenasSemana3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Ventana2 : ContentPage
+    {
+        public Ventana2(string user, string pass)
+        {
+            InitializeComponent();
+            lblUser.Text = user;
+            lblPassword.Text = pass;
+        }
+    }
+}
